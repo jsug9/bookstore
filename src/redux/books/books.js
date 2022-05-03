b467f18135387ef66b96fbc477bfb6d026156f9a
Augusto <jesus_augga_99@icloud.com>
2022-05-03 12:04:02 -0500

    change book to payload for better understanding

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,21 +1,23 @@
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
 
-const addBook = (book) => ({
+const initialState = [];
+
+const addBook = (payload) => ({
   type: ADD_BOOK,
-  book,
+  payload,
 });
-const removeBook = (book) => ({
+const removeBook = (payload) => ({
   type: REMOVE_BOOK,
-  book,
+  payload,
 });
 
-const booksReducer = ((state = [], action) => {
+const booksReducer = ((state = initialState, action) => {
   switch (action.type) {
     case ADD_BOOK:
-      return [...state, action.book];
+      return [...state, action.payload];
     case REMOVE_BOOK:
-      return state.filter((book) => book !== action.book);
+      return state.filter((payload) => payload !== action.payload);
     default:
       return state;
   }
