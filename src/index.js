29b0cb1b5f27583d1b88af206a0dfb770db0ecbf
Augusto <jesus_augga_99@icloud.com>
2022-05-04 15:47:53 -0500

    minor change in index.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,11 @@ import './index.scss';
 import App from './App';
 import store from './redux/configureStore';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'),
 );
